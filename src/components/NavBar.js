142e94789f9e1556b23e73d6a0d41e9c51363b0d
Andreas Baumgartner <tech@andreasbaumgartner.ch>
2022-07-04 13:27:33 +0200

    Template finish

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,17 +10,16 @@ export const NavBar = () => {
   const [scrolled, seScrolled] = useState(false);
 
   useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        seScrolled(true);
-      } else {
-        seScrolled(false);
-      }
+    const onScroll = () => {};
+    if (window.scrollY > 50) {
+      seScrolled(true);
+    } else {
+      seScrolled(false);
+    }
 
-      window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-      return () => window.removeEventListener("scroll", onScroll);
-    };
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
   const onUpdateActiveLink = (value) => {
